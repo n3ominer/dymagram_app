7a6b7d6a73d8aeae1b8f3bbc24503d6ed26fc961
Ramzy Kermad <kermad.r@sncf.fr>
2024-10-27 14:43:28 +0100

    Added navigation + story pause in user stories

--- app/src/main/java/com/example/dymagram/pages/UserStoryDetailActivity.kt
+++ app/src/main/java/com/example/dymagram/pages/UserStoryDetailActivity.kt
@@ -1,8 +1,11 @@
 package com.example.dymagram.pages
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -29,6 +32,10 @@ class UserStoryDetailActivity : AppCompatActivity() {
     private lateinit var storyUserNameTextView: TextView
     private lateinit var storyUserPseudoTextView: TextView
 
+    private lateinit var previousStoryClickableView: View
+    private lateinit var nextStoryClickableView: View
+
+    private var storyPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +50,7 @@ class UserStoryDetailActivity : AppCompatActivity() {
         getIntentData()
         setUpViews()
         fillViews()
+        setUpStoryNavigationViews()
     }
 
     private fun getIntentData() {
@@ -63,11 +71,45 @@ class UserStoryDetailActivity : AppCompatActivity() {
         Glide.with(this)
             .load("https://robohash.org/${this.userName}")
             .into(this.storyUserProfileImageView)
-        Glide.with(this)
-            .load(this.storiesContentUrl.first())
-            .into(this.storyContentImageView)
+        displaySotryContent()
         this.storyUserNameTextView.text = this.userName
         this.storyUserPseudoTextView.text = this.userPseudo
     }
 
+    private fun setUpStoryNavigationViews() {
+        this.previousStoryClickableView = findViewById(R.id.previous_story_view)
+        this.previousStoryClickableView.setOnClickListener {
+            if (this.storyPosition - 1 >= 0) {
+                this.storyPosition -= 1
+                this.displaySotryContent()
+            } else {
+                finish()
+            }
+        }
+        this.previousStoryClickableView.setOnLongClickListener {
+            Toast.makeText(this, "Écoulement du temps de la story mis en pause", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        this.nextStoryClickableView = findViewById(R.id.next_story_view)
+        this.nextStoryClickableView.setOnClickListener {
+            if (this.storyPosition + 1 < this.storiesContentUrl.size) {
+                this.storyPosition += 1
+                this.displaySotryContent()
+            } else {
+                finish()
+            }
+        }
+        this.nextStoryClickableView.setOnLongClickListener {
+            Toast.makeText(this, "Écoulement du temps de la story mis en pause", Toast.LENGTH_LONG).show()
+            true
+        }
+    }
+
+
+    private fun displaySotryContent() {
+        Glide.with(this)
+            .load(this.storiesContentUrl[this.storyPosition])
+            .into(this.storyContentImageView)
+    }
 }
