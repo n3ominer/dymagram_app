8db9ce0c9da414d94d88d7e61a049046a73d5f6c
Ramzy Kermad <kermad.r@sncf.fr>
2024-12-03 16:38:48 +0100

    Implemented Firebase Firestore to save data from posts

--- app/src/main/java/com/example/dymagram/repositories/GlobalDataRepository.kt
+++ app/src/main/java/com/example/dymagram/repositories/GlobalDataRepository.kt
@@ -1,12 +1,13 @@
 package com.example.dymagram.repositories
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.dymagram.data.model.GlobalDataModel
 import com.example.dymagram.network.dto.GlobalModelDto
 import com.example.dymagram.network.mapper.mapGlobalDataDtoToGlobalDataModel
 import com.example.dymagram.network.services.GlobalDataService
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import io.reactivex.rxjava3.subjects.PublishSubject
 
 import retrofit2.Call
@@ -16,6 +17,8 @@ import retrofit2.Response
 class GlobalDataRepository(private val globalService: GlobalDataService) {
 
     val globalData : PublishSubject<GlobalDataModel> = PublishSubject.create()
+
+    private val remoteDataBase = Firebase.firestore
 
     fun getAllData() {
         val call = globalService.getAllData()
@@ -31,7 +34,9 @@ class GlobalDataRepository(private val globalService: GlobalDataService) {
                     val responseBody = response.body()
                     this@GlobalDataRepository.globalData.onNext(
                         responseBody?.let {
-                            mapGlobalDataDtoToGlobalDataModel(it)
+                            val globalDataModel = mapGlobalDataDtoToGlobalDataModel(it)
+                            saveDataToFirestore(globalDataModel)
+                            globalDataModel
                         }
                     )
                 } else {
@@ -48,6 +53,25 @@ class GlobalDataRepository(private val globalService: GlobalDataService) {
             }
 
         })
+    }
+
+    private fun saveDataToFirestore(data: GlobalDataModel) {
+        data.posts.forEach {
+            val postsData = hashMapOf(
+                "user_name" to it.username,
+                "user_profile_picture" to "https://robohash.org/${it.user_id}",
+                "user_id" to it.user_id,
+            )
+
+            remoteDataBase.collection("user_feed_posts")
+                .add(postsData)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("Firebase Firestore", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("Firebase Firestore", "Error adding document", e)
+                }
+        }
     }
 
 
